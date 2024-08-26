41fd532ce909081d2006b87adfb2e9801d097aa8
gaiijinn <vladruban8@gmail.com>
2024-08-26 15:02:42 +0300

    new class NoneReturner in service

--- map_project/map_apps/achievements/services/achievement_progress_updater.py
+++ map_project/map_apps/achievements/services/achievement_progress_updater.py
@@ -1,19 +1,42 @@
 from django.db.models import F
 from django.contrib.auth import get_user_model
 from ..models import Achievements, AchievementsProgressStatus
+from django.core.exceptions import ObjectDoesNotExist
+
+from abc import ABC, abstractmethod
 
 
-class AchievementGetter:
-    def get_achievement(self, achievements_id):
-        return Achievements.objects.get(id=achievements_id)
+class NoneReturner:
+    def get_object_or_none(self, model_class, **kwargs):
+        try:
+            return model_class.objects.get(**kwargs)
+        except ObjectDoesNotExist:
+            return None
 
 
-class UserGetter:
+class AchievementGetter(NoneReturner):
+    def get_achievement(self, achievement_id):
+        return super().get_object_or_none(Achievements, id=achievement_id)
+
+
+class UserGetter(NoneReturner):
     def get_user(self, users_id):
-        return get_user_model().objects.get(id=users_id)
+        return super().get_object_or_none(get_user_model(), id=users_id)
 
 
 class AchievementProgressController:
+    """
+    Service for updating the progress of achievements for users.
+
+    This class is responsible for updating the progress of a specific achievement for a user.
+    It retrieves the achievement and user objects using the provided IDs, and then increments
+    the progress of the achievement for the user.
+
+    This class is intended to be used within controllers to handle the update logic
+    for achievement progress. After successful updating, a post-save signal will be triggered on
+    AchievementProgressStatus model, which then invoke a task for changing the boolean field and level calculating.
+    """
+
     def __init__(self, achievements_id, users_id):
         self.achievements_id = achievements_id
         self.users_id = users_id
